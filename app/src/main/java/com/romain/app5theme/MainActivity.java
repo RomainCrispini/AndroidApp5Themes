6eb6b7400752fb57e2c3cf184acc51429e02a95a
RomainCrispini <romain.crispini@gmail.com>
2020-02-11 10:55:14 +0100

    Android App Changement Themes Essai

---
+++ app/src/main/java/com/romain/app5theme/MainActivity.java
@@ -0,0 +1,14 @@
+package com.romain.app5theme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
